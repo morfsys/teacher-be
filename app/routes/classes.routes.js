1d9cecd8bd0e965915d3096c55a6e9089b3af4c8
Nidhil <nidil931@morfsys.com>
2019-12-02 22:29:59 +0530

    Updated

--- app/routes/classes.routes.js
+++ app/routes/classes.routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const classes = require("../controllers/classes.controller")();
 
 router.post("/", classes.create);
+router.get("/", classes.get);
 
 module.exports = router;
